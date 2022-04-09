367d317ab9af6ffba951d3c30373cc9070864afb
patrykzapala <80274702+patrykzapala@users.noreply.github.com>
2022-04-09 12:30:48 +0200

    Add files via upload

---
+++ DIM_Customers.sql
@@ -0,0 +1,41 @@
+-- Cleansed DIM_Customers Table --
+SELECT 
+	c.CustomerKey AS CustomerKey
+      --,[GeographyKey]
+      --,[CustomerAlternateKey]
+      --,[Title]
+      , c.FirstName AS [First Name]
+      --,[MiddleName]
+      , c.LastName AS [Last Name]
+	  , c.FirstName + ' ' + c.LastName AS [Full Name]
+      --,[NameStyle]
+      --,[BirthDate]
+      --,[MaritalStatus]
+      --,[Suffix]
+      , CASE c.Gender 
+			WHEN 'M' THEN 'Male'
+			WHEN 'F' THEN 'Female'
+			END AS Gender
+      --,[EmailAddress]
+      --,[YearlyIncome]
+      --,[TotalChildren]
+      --,[NumberChildrenAtHome]
+      --,[EnglishEducation]
+      --,[SpanishEducation]
+      --,[FrenchEducation]
+      --,[EnglishOccupation]
+      --,[SpanishOccupation]
+      --,[FrenchOccupation]
+      --,[HouseOwnerFlag]
+      --,[NumberCarsOwned]
+      --,[AddressLine1]
+      --,[AddressLine2]
+      --,[Phone]
+      , c.DateFirstPurchase AS DateFirstPurchase 
+      --,[CommuteDistance]
+	  , g.City AS [Customer City] 
+  FROM 
+	AdventureWorksDW2019.dbo.DimCustomer AS c
+	LEFT JOIN AdventureWorksDW2019.dbo.DimGeography AS g ON g.GeographyKey = c.GeographyKey
+  ORDER BY
+	CustomerKey ASC 
